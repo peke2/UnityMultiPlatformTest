c222b48ba13ed46c4816e9202059e1739f7d5fc6
Masaki Abe <abem@dc4.so-net.ne.jp>
2018-02-09 16:56:21 +0900

    サンプルで作ったdllを追加。 それを参照するスクリプトも追加。実行時にdllが見つからないと言われる。

---
+++ Assets/Scripts/Importer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Runtime.InteropServices;
+
+public class Importer : MonoBehaviour {
+
+	[DllImport ("packed")]
+	private static extern IntPtr GetToken();
+
+	// Use this for initialization
+	void Start () {
+		IntPtr ptr = GetToken();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
