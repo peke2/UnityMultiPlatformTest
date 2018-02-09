8568d6661aee7dd57a2fa2a4426d256ad8ecfc2d
Masaki Abe <abem@dc4.so-net.ne.jp>
2018-02-09 20:54:16 +0900

    ネイティブプラグインの読み込みが出来た。 Unity Editorが64bitなら、Playした時に64bitのdllしか読めないっぽい。 試しに別の関数を追加して、それぞれの戻り値を確認してみた。

--- UnityProject/Assets/Scripts/Importer.cs
+++ UnityProject/Assets/Scripts/Importer.cs
@@ -7,12 +7,23 @@ using System.Runtime.InteropServices;
 
 public class Importer : MonoBehaviour {
 
-	[DllImport ("packed")]
+	//[DllImport ("packed")]
+	[DllImport ("PluginSample")]	//64bit版ならDLL Not Foundは出ない
 	private static extern IntPtr GetToken();
+
+	[DllImport ("PluginSample")]
+	private static extern IntPtr Say();
 
 	// Use this for initialization
 	void Start () {
 		IntPtr ptr = GetToken();
+		byte[] buff = new byte[16];
+		Marshal.Copy(ptr, buff, 0,16);
+
+		ptr = Say();
+		string message = Marshal.PtrToStringAnsi(ptr);
+		Debug.Log(message);
+
 	}
 	
 	// Update is called once per frame
